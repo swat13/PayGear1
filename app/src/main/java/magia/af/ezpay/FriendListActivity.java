e68f91c93077f460082538a80cea704871c276bc
Ali Freidouni <ali_freidouni@yahoo.com>
2016-10-28 17:27:28 +0330

    alif

--- app/src/main/java/magia/af/ezpay/FriendListActivity.java
+++ app/src/main/java/magia/af/ezpay/FriendListActivity.java
@@ -12,37 +12,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import magia.af.ezpay.Parser.DOMParser;
 import magia.af.ezpay.Parser.RSSFeed;
+import magia.af.ezpay.Parser.RSSItem;
 import magia.af.ezpay.Utilities.LocalPersistence;
 import magia.af.ezpay.helper.GetContact;
-import magia.af.ezpay.modules.ContactItem;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class FriendListActivity extends BaseActivity {
 
-  RSSFeed _feed;
-  ListAdapter listAdapter;
-  RecyclerView recBills;
-  ListAdapter adapter;
-  int pos = 0;
+    RSSFeed _feed;
+    RecyclerView recBills;
+    ListAdapter adapter;
 
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_friend_list);
-    recBills = (RecyclerView) findViewById(R.id.contact_recycler);
-    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_list);
+        recBills = (RecyclerView) findViewById(R.id.contact_recycler);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
-    StrictMode.setThreadPolicy(policy);
-    new fillContact().execute();
+        StrictMode.setThreadPolicy(policy);
+        new fillContact().execute(new GetContact().getContact(FriendListActivity.this));
 
 //        if (_feed != null) {
 //            adapter = new ListAdapter();
@@ -54,43 +45,39 @@ public class FriendListActivity extends BaseActivity {
 //            adapter.notifyDataSetChanged();
 //        }
 
-  }
-
-  public class FeedViewHolder extends RecyclerView.ViewHolder {
-
-    TextView contactName;
-    ImageView contactImage;
-    ImageView contactStat;
-
-    public FeedViewHolder(View v) {
-      super(v);
-      contactName = (TextView) v.findViewById(R.id.name_text);
-      contactImage = (ImageView) v.findViewById(R.id.contact_img);
-      contactStat = (ImageView) v.findViewById(R.id.status_circle);
-
-    }
-  }
-
-  public class ListAdapter extends RecyclerView.Adapter<FeedViewHolder> {
-    ArrayList<ContactItem> contactItems;
-
-    public ListAdapter(ArrayList<ContactItem> contactItems) {
-      this.contactItems = contactItems;
     }
 
-    @Override
-    public int getItemCount() {
-      return contactItems.size();
+    public class FeedViewHolder extends RecyclerView.ViewHolder {
+
+        TextView contactName;
+        ImageView contactImage;
+        ImageView contactStat;
+
+        public FeedViewHolder(View v) {
+            super(v);
+            contactName = (TextView) v.findViewById(R.id.name_text);
+            contactImage = (ImageView) v.findViewById(R.id.contact_img);
+            contactStat = (ImageView) v.findViewById(R.id.status_circle);
+
+        }
     }
 
-    @Override
-    public void onBindViewHolder(final FeedViewHolder FeedViewHolder, final int position) {
+    public class ListAdapter extends RecyclerView.Adapter<FeedViewHolder> {
 
-      FeedViewHolder.contactName.setText(contactItems.get(position).getContactName());
+        public ListAdapter() {
+        }
 
-//            final RSSItem fe = _feed.getItem(position);
+        @Override
+        public int getItemCount() {
+            return _feed.getItemCount();
+        }
 
-//            FeedViewHolder.contactName.setText(fe.getContactName());
+        @Override
+        public void onBindViewHolder(final FeedViewHolder FeedViewHolder, final int position) {
+
+            final RSSItem fe = _feed.getItem(position);
+
+            FeedViewHolder.contactName.setText(fe.getContactName());
 //        FeedViewHolder.contactImage.setImageDrawable(fe.getContactImg());
 
             /*if (fe.isContactStatus()) {
@@ -100,55 +87,57 @@ public class FriendListActivity extends BaseActivity {
                 FeedViewHolder.contactStat.setImageResource(R.drawable.circle_bg_offline_stat);*/
 
 
-    }
+        }
 
-    @Override
-    public FeedViewHolder onCreateViewHolder(final ViewGroup viewGroup, final int position) {
-      View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.contact_item, viewGroup, false);
-      return new FeedViewHolder(itemView);
-    }
-
-  }
-
-  private class fillContact extends AsyncTask<ArrayList<ContactItem>, Void, ArrayList<ContactItem>> {
-
-    @Override
-    protected void onPreExecute() {
-      super.onPreExecute();
+        @Override
+        public FeedViewHolder onCreateViewHolder(final ViewGroup viewGroup, final int position) {
+            View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.contact_item, viewGroup, false);
+            return new FeedViewHolder(itemView);
+        }
 
     }
 
-    @Override
-    protected ArrayList<ContactItem> doInBackground(ArrayList<ContactItem>... params) {
-      DOMParser domParser = new DOMParser(getSharedPreferences("EZpay", 0).getString("token", ""));
-      return domParser.getRegisteredContactFromServer(new GetContact().getContact(FriendListActivity.this));
+    private class fillContact extends AsyncTask<String, Void, RSSFeed> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected RSSFeed doInBackground(String... params) {
+            DOMParser domParser = new DOMParser(getSharedPreferences("EZpay", 0).getString("token", ""));
+            return domParser.getContact(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(RSSFeed result) {
+            if (result != null) {
+                if (_feed == null || _feed.getItemCount() == 0) {
+                    _feed = result;
+//                    Log.e("000000000", "onPostExecute: " + result.getItem(2).getTelNo());
+                    adapter = new ListAdapter();
+                    recBills.setAdapter(adapter);
+                    LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+                    llm.setOrientation(LinearLayoutManager.VERTICAL);
+                    recBills.setNestedScrollingEnabled(true);
+                    recBills.setLayoutManager(llm);
+                    adapter.notifyDataSetChanged();
+                } else {
+                    _feed = result;
+                    adapter.notifyDataSetChanged();
+                }
+                new LocalPersistence().writeObjectToFile(getApplicationContext(), _feed, "Contact_List");
+            }
+            else
+                Toast.makeText(FriendListActivity.this, "problem in connection!", Toast.LENGTH_SHORT).show();
+
+        }
+
     }
 
-    @Override
-    protected void onPostExecute(ArrayList<ContactItem> result) {
-      if (result != null) {
-        adapter = new ListAdapter(result);
-        recBills.setAdapter(adapter);
-//                if (_feed == null || _feed.getItemCount() == 0) {
-//                    _feed = result;
-////                    Log.e("000000000", "onPostExecute: " + result.getItem(2).getTelNo());
-//                    adapter = new ListAdapter();
-//                    recBills.setAdapter(adapter);
-        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recBills.setNestedScrollingEnabled(true);
-        recBills.setLayoutManager(llm);
-        adapter.notifyDataSetChanged();
-//                } else {
-//                    _feed = result;
-//                    adapter.notifyDataSetChanged();
-//                }
-        new LocalPersistence().writeObjectToFile(getApplicationContext(), _feed, "Contact_List");
-      } else {
-        Toast.makeText(getApplicationContext(), "مشکل در برقراری ارتباط!", Toast.LENGTH_SHORT).show();
-      }
+    private void removeRepeatContacts() {
+
     }
-  }
-
-
 }
--- app/src/main/java/magia/af/ezpay/helper/GetContact.java
+++ app/src/main/java/magia/af/ezpay/helper/GetContact.java
@@ -4,6 +4,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.provider.ContactsContract;
+import android.util.ArrayMap;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -16,54 +17,61 @@ import org.json.JSONObject;
 
 public class GetContact {
 
-  private static final String TAG = "TAG";
+    private static final String TAG = "TAG";
+    private ArrayMap<String, Boolean> stringArrayMap = new ArrayMap<>();
 
-  public String getContact(Context context) {
-    JSONArray jsonArray = null;
-    ContentResolver cr = context.getContentResolver();
-    Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-      null, null, null, null);
+    public String getContact(Context context) {
+        JSONArray jsonArray = null;
+        ContentResolver cr = context.getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
 
-    if (cur.getCount() > 0) {
-      jsonArray = new JSONArray();
-      int count = 0;
-      while (cur.moveToNext()) {
-        String id = cur.getString(
-          cur.getColumnIndex(ContactsContract.Contacts._ID));
-        String name = cur.getString(cur.getColumnIndex(
-          ContactsContract.Contacts.DISPLAY_NAME));
+        if (cur.getCount() > 0) {
+            jsonArray = new JSONArray();
+            int count = 0;
+            while (cur.moveToNext()) {
+                String id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
 
-        if (cur.getInt(cur.getColumnIndex(
-          ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
-          Cursor pCur = cr.query(
-            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-            null,
-            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-            new String[]{id}, null);
-          while (pCur.moveToNext()) {
-            String phoneNo = pCur.getString(pCur.getColumnIndex(
-              ContactsContract.CommonDataKinds.Phone.NUMBER));
-            JSONObject jsonObject = new JSONObject();
-            if (phoneNo.contains(" ")) {
-              phoneNo = phoneNo.replace(" ", "");
+                if (cur.getInt(cur.getColumnIndex(
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        if (phoneNo.contains(" ")) {
+                            phoneNo = phoneNo.replace(" ", "");
+                        }
+                        if (phoneNo.contains("+98")) {
+                            phoneNo = phoneNo.replace("+98", "0");
+                        }
+                        try {
+                            Log.e("00000000", "getContact: " + phoneNo);
+                            if (!stringArrayMap.containsKey(phoneNo)) {
+                                JSONObject jsonObject = new JSONObject();
+                                Log.e("1111111111", "getContact: " + phoneNo);
+                                jsonObject.put("t", name);
+                                jsonObject.put("m", phoneNo);
+                                jsonArray.put(count, jsonObject);
+                                stringArrayMap.put(phoneNo, true);
+                                count++;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    pCur.close();
+                }
             }
-            if (phoneNo.contains("+98")) {
-              phoneNo = phoneNo.replace("+98", "0");
-            }
-            try {
-              jsonObject.put("t", name);
-              jsonObject.put("m", phoneNo);
-              jsonArray.put(count, jsonObject);
-            } catch (JSONException e) {
-              e.printStackTrace();
-            }
-            count++;
-          }
-          pCur.close();
         }
-      }
+        Log.i("JSON CONTACT", jsonArray.toString());
+        return jsonArray != null ? jsonArray.toString() : null;
     }
-    Log.i("JSON CONTACT", jsonArray.toString());
-    return jsonArray != null ? jsonArray.toString() : null;
-  }
 }
