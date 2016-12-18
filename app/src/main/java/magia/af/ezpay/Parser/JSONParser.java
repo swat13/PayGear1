270fa3fb0007f7d031ff45fcc16f79d57c333f0a
Erfan Taghvaei <etaghvaei0098@gmail.com>
2016-12-18 09:21:39 +0330

    ImageMaker class

--- app/src/main/java/magia/af/ezpay/Parser/JSONParser.java
+++ app/src/main/java/magia/af/ezpay/Parser/JSONParser.java
@@ -17,181 +17,181 @@ import java.net.URL;
  */
 
 public class JSONParser {
-  private int resCode;
-  private int connectionTimeOut;
-  private int readTimeOut;
-  private String url;
-  private String requestMethod;
-  private boolean output;
-  private boolean input;
-  private boolean userInteraction;
-  private String requestPropertyKey;
-  private String requestPropertyValue;
-  private String jsonValue;
-  public static final String GET = "GET";
-  public static final String POST = "POST";
-  public static final String PUT = "PUT";
-  public static final String PATCH = "PATCH";
-  public static final String DELETE = "DELETE";
-  public static final String COPY = "COPY";
-  public static final String HEAD = "HEAD";
-  public static final String OPTIONS = "OPTIONS";
-  public static final String LINK = "LINK";
-  public static final String UNLINK = "UNLINK";
-  public static final String PURGE = "PURGE";
-  public static final String LOCK = "LOCK";
-  public static final String UNLOCK = "UNLOCK";
-  public static final String PROPFIND = "PROPFIND";
-  public static final String VIEW = "VIEW";
+    private int resCode;
+    private int connectionTimeOut;
+    private int readTimeOut;
+    private String url;
+    private String requestMethod;
+    private boolean output;
+    private boolean input;
+    private boolean userInteraction;
+    private String requestPropertyKey;
+    private String requestPropertyValue;
+    private String jsonValue;
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String PUT = "PUT";
+    public static final String PATCH = "PATCH";
+    public static final String DELETE = "DELETE";
+    public static final String COPY = "COPY";
+    public static final String HEAD = "HEAD";
+    public static final String OPTIONS = "OPTIONS";
+    public static final String LINK = "LINK";
+    public static final String UNLINK = "UNLINK";
+    public static final String PURGE = "PURGE";
+    public static final String LOCK = "LOCK";
+    public static final String UNLOCK = "UNLOCK";
+    public static final String PROPFIND = "PROPFIND";
+    public static final String VIEW = "VIEW";
 
-  public JSONParser() {
+    public JSONParser() {
 
-  }
-
-  public String connect(String url) {
-    this.url = url;
-    StringBuilder sb = null;
-    try {
-      URL mUrl = new URL(url);
-      HttpURLConnection connection = (HttpURLConnection) mUrl.openConnection();
-      InputStream in = connection.getInputStream();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-      sb = new StringBuilder();
-
-      String line = "";
-      try {
-        while ((line = reader.readLine()) != null) {
-          sb.append(line);
-        }
-      } catch (IOException e) {
-        e.printStackTrace();
-      } finally {
-        try {
-          in.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
     }
-    assert sb != null;
-    return sb.toString();
-  }
 
-  public String connect(String url, String requestMethod, int readTimeOut, int connectionTimeOut, String authorization, String json) {
-    StringBuilder sb = new StringBuilder();
-    try {
-      URL apiUrl = new URL(url);
-      HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
-      connection.setRequestMethod(requestMethod);
-      connection.setConnectTimeout(connectionTimeOut);
-      connection.setReadTimeout(readTimeOut);
-      connection.setRequestProperty("Content-Type", "application/json");
-      if (authorization != null) {
-        connection.setRequestProperty("Authorization", authorization);
-      }
-
-      if (json != null) {
-        OutputStream outputStream = connection.getOutputStream();
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-        writer.write(json);
-        writer.flush();
-        writer.close();
-        outputStream.close();
-        setResCode(connection.getResponseCode());
-      }
-      if (connection.getInputStream() != null) {
-        InputStream in = connection.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-        String line = null;
+    public String connect(String url) {
+        this.url = url;
+        StringBuilder sb = null;
         try {
-          while ((line = reader.readLine()) != null) {
-            sb.append(line);
-          }
+            URL mUrl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) mUrl.openConnection();
+            InputStream in = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            sb = new StringBuilder();
+
+            String line = "";
+            try {
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (IOException e) {
-          e.printStackTrace();
-        } finally {
-          try {
-            in.close();
-          } catch (IOException e) {
             e.printStackTrace();
-          }
         }
-      } else {
-        return "";
-      }
-      if (connection.getResponseCode() == 200 || connection.getResponseCode() == 201) {
-        if (sb.toString() != null || !sb.toString().isEmpty()) {
-          return sb.toString();
-        } else {
-          return "";
+        assert sb != null;
+        return sb.toString();
+    }
+
+    public String connect(String url, String requestMethod, int readTimeOut, int connectionTimeOut, String authorization, String json) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            URL apiUrl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+            connection.setRequestMethod(requestMethod);
+            connection.setConnectTimeout(connectionTimeOut);
+            connection.setReadTimeout(readTimeOut);
+            connection.setRequestProperty("Content-Type", "application/json");
+            if (authorization != null) {
+                connection.setRequestProperty("Authorization", authorization);
+            }
+
+            if (json != null) {
+                OutputStream outputStream = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                writer.write(json);
+                writer.flush();
+                writer.close();
+                outputStream.close();
+                setResCode(connection.getResponseCode());
+            }
+            if (connection.getInputStream() != null) {
+                InputStream in = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String line = null;
+                try {
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                return "";
+            }
+            if (connection.getResponseCode() == 200 || connection.getResponseCode() == 201) {
+                if (sb.toString() != null || !sb.toString().isEmpty()) {
+                    return sb.toString();
+                } else {
+                    return "";
+                }
+            } else {
+                return "Error With Code " + connection.getResponseCode();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-      } else {
-        return "Error With Code " + connection.getResponseCode();
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return sb.toString();
-  }
-
-  public int getResultCode() {
-    return resCode;
-  }
-
-  private void setResCode(int resCode) {
-    this.resCode = resCode;
-  }
-
-  public void setConnectionTimeOut(int connectionTimeOut) {
-    this.connectionTimeOut = connectionTimeOut;
-  }
-
-  public void setReadTimeOut(int readTimeOut) {
-    this.readTimeOut = readTimeOut;
-  }
-
-  public void setOutput(boolean output) {
-    this.output = output;
-  }
-
-  public void setInput(boolean input) {
-    this.input = input;
-  }
-
-  public void setUserInteraction(boolean userInteraction) {
-    this.userInteraction = userInteraction;
-  }
-
-  public void setRequestPropertyKey(String requestPropertyKey) {
-    this.requestPropertyKey = requestPropertyKey;
-  }
-
-  public void setRequestPropertyValue(String requestPropertyValue) {
-    this.requestPropertyValue = requestPropertyValue;
-  }
-
-  public class Connect extends AsyncTask<Void, Void, String> {
-
-    @Override
-    protected String doInBackground(Void... params) {
-      return null;
+        return sb.toString();
     }
 
-    @Override
-    protected void onPostExecute(String s) {
-      setJsonValue(s);
-      super.onPostExecute(s);
+    public int getResultCode() {
+        return resCode;
     }
-  }
 
-  private String getJsonValue() {
-    return jsonValue;
-  }
+    private void setResCode(int resCode) {
+        this.resCode = resCode;
+    }
 
-  public void setJsonValue(String jsonValue) {
-    this.jsonValue = jsonValue;
-  }
+    public void setConnectionTimeOut(int connectionTimeOut) {
+        this.connectionTimeOut = connectionTimeOut;
+    }
+
+    public void setReadTimeOut(int readTimeOut) {
+        this.readTimeOut = readTimeOut;
+    }
+
+    public void setOutput(boolean output) {
+        this.output = output;
+    }
+
+    public void setInput(boolean input) {
+        this.input = input;
+    }
+
+    public void setUserInteraction(boolean userInteraction) {
+        this.userInteraction = userInteraction;
+    }
+
+    public void setRequestPropertyKey(String requestPropertyKey) {
+        this.requestPropertyKey = requestPropertyKey;
+    }
+
+    public void setRequestPropertyValue(String requestPropertyValue) {
+        this.requestPropertyValue = requestPropertyValue;
+    }
+
+    public class Connect extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            setJsonValue(s);
+            super.onPostExecute(s);
+        }
+    }
+
+    private String getJsonValue() {
+        return jsonValue;
+    }
+
+    public void setJsonValue(String jsonValue) {
+        this.jsonValue = jsonValue;
+    }
 }
--- app/src/main/java/magia/af/ezpay/fragments/Profile.java
+++ app/src/main/java/magia/af/ezpay/fragments/Profile.java
@@ -3,6 +3,7 @@ package magia.af.ezpay.Fragments;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -19,6 +20,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
@@ -28,6 +30,7 @@ import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -53,6 +56,8 @@ import magia.af.ezpay.MainActivity;
 import magia.af.ezpay.Parser.ChatListItem;
 import magia.af.ezpay.Parser.Parser;
 import magia.af.ezpay.R;
+import magia.af.ezpay.helper.ExifUtils;
+import magia.af.ezpay.helper.ImageMaker;
 
 
 /**
@@ -100,10 +105,10 @@ public class Profile extends Fragment {
             phoneNumber.setText(getActivity().getSharedPreferences("EZpay", 0).getString("phoneNumber", ""));
             Ipath = getActivity().getSharedPreferences("EZpay", 0).getString("Ipath", "");
 
-            Log.e("ImageString","http://new.opaybot.ir" + Ipath.replace("\"","") );
+            Log.e("ImageString", "http://new.opaybot.ir" + Ipath.replace("\"", ""));
 
             Glide.with(getActivity())
-                    .load("http://new.opaybot.ir" + Ipath.replace("\"",""))
+                    .load("http://new.opaybot.ir" + Ipath.replace("\"", ""))
                     .asBitmap()
                     .centerCrop()
                     .placeholder(R.drawable.pic_profile)
@@ -115,7 +120,6 @@ public class Profile extends Fragment {
                             userAvatar.setImageDrawable(circularBitmapDrawable);
                         }
                     });
-
 
 
         }
@@ -170,57 +174,34 @@ public class Profile extends Fragment {
         return rootView;
     }
 
-    public void checkPermissions(){
-        ActivityCompat.requestPermissions(getActivity(),new String[]{android.Manifest.permission.CAMERA},0);
+    public void checkPermissions() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.CAMERA}, 0);
     }
 
     Bitmap thumbnail;
 
-    private File onCaptureImageResult(Intent data) {
-        thumbnail = (Bitmap) data.getExtras().get("data");
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        File destination = new File(Environment.getExternalStorageDirectory(),
-                System.currentTimeMillis() + ".jpg");
-        FileOutputStream fo;
-        try {
-            destination.createNewFile();
-            fo = new FileOutputStream(destination);
-            fo.write(bytes.toByteArray());
-            fo.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private String onCaptureImageResult(Intent data) {
+        if (data!=null) {
+            thumbnail = (Bitmap) data.getExtras().get("data");
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+            byte[] byteArray = bytes.toByteArray();
+            String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+            return encoded;
         }
-        return destination;
+        else
+            return null;
     }
 
     @SuppressWarnings("deprecation")
-    private File onSelectFromGalleryResult(Intent data) {
+    private String onSelectFromGalleryResult(Intent data) {
+
+
         thumbnail = null;
         if (data != null) {
-            try {
-                thumbnail = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
-                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-                thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
-                File destination = new File(Environment.getExternalStorageDirectory(),
-                        System.currentTimeMillis() + ".jpg");
-                FileOutputStream fo;
-                try {
-                    destination.createNewFile();
-                    fo = new FileOutputStream(destination);
-                    fo.write(bytes.toByteArray());
-                    fo.close();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return destination;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+            ImageMaker imageMaker=new ImageMaker(getActivity().getApplicationContext(),data);
+            return imageMaker.onSelectFromGalleryResult();
         }
         return null;
     }
@@ -246,7 +227,7 @@ public class Profile extends Fragment {
         }
     }
 
-    private class AsyncInsertUserImage extends AsyncTask<File, Void, String> {
+    private class AsyncInsertUserImage extends AsyncTask<String, Void, String> {
 
         @Override
         protected void onPreExecute() {
@@ -258,7 +239,7 @@ public class Profile extends Fragment {
         }
 
         @Override
-        protected String doInBackground(File... params) {
+        protected String doInBackground(String... params) {
             Parser parser = new Parser(getActivity().getSharedPreferences("EZpay", 0).getString("token", ""));
             try {
                 return parser.changeUserImage(params[0]);
@@ -272,7 +253,7 @@ public class Profile extends Fragment {
         protected void onPostExecute(String result) {
             if (!result.equals("Error")) {
                 Glide.with(getActivity())
-                        .load("http://new.opaybot.ir" + result.replace("\"",""))
+                        .load("http://new.opaybot.ir" + result.replace("\"", ""))
                         .asBitmap()
                         .centerCrop()
                         .placeholder(R.drawable.pic_profile)
@@ -292,189 +273,6 @@ public class Profile extends Fragment {
 
             ((MainActivity) getActivity()).waitingDialog.setVisibility(View.GONE);
         }
-    }
-
-    public Bitmap getRoundedShape(Bitmap scaleBitmapImage) {
-        int targetWidth = 300;
-        int targetHeight = 300;
-        Bitmap targetBitmap = Bitmap.createBitmap(targetWidth,
-                targetHeight, Bitmap.Config.ARGB_8888);
-
-        Canvas canvas = new Canvas(targetBitmap);
-        Path path = new Path();
-        path.addCircle(((float) targetWidth - 1) / 2,
-                ((float) targetHeight - 1) / 2,
-                (Math.min(((float) targetWidth),
-                        ((float) targetHeight)) / 2),
-                Path.Direction.CCW);
-
-        canvas.clipPath(path);
-        Bitmap sourceBitmap = scaleBitmapImage;
-        canvas.drawBitmap(sourceBitmap,
-                new Rect(0, 0, sourceBitmap.getWidth(),
-                        sourceBitmap.getHeight()),
-                new Rect(0, 0, targetWidth, targetHeight), null);
-        return targetBitmap;
-    }
-
-    public String compressImage(String imageUri) {
-
-        String filePath = getRealPathFromURI(imageUri);
-        Bitmap scaledBitmap = null;
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-
-//      by setting this field as true, the actual bitmap pixels are not loaded in the memory. Just the bounds are loaded. If
-//      you try the use the bitmap here, you will get null.
-        options.inJustDecodeBounds = true;
-        Bitmap bmp = BitmapFactory.decodeFile(filePath, options);
-
-        int actualHeight = options.outHeight;
-        int actualWidth = options.outWidth;
-
-//      max Height and width values of the compressed image is taken as 816x612
-
-        float maxHeight = 816.0f;
-        float maxWidth = 612.0f;
-        float imgRatio = actualWidth / actualHeight;
-        float maxRatio = maxWidth / maxHeight;
-
-//      width and height values are set maintaining the aspect ratio of the image
-
-        if (actualHeight > maxHeight || actualWidth > maxWidth) {
-            if (imgRatio < maxRatio) {
-                imgRatio = maxHeight / actualHeight;
-                actualWidth = (int) (imgRatio * actualWidth);
-                actualHeight = (int) maxHeight;
-            } else if (imgRatio > maxRatio) {
-                imgRatio = maxWidth / actualWidth;
-                actualHeight = (int) (imgRatio * actualHeight);
-                actualWidth = (int) maxWidth;
-            } else {
-                actualHeight = (int) maxHeight;
-                actualWidth = (int) maxWidth;
-
-            }
-        }
-
-//      setting inSampleSize value allows to load a scaled down version of the original image
-
-        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight);
-
-//      inJustDecodeBounds set to false to load the actual bitmap
-        options.inJustDecodeBounds = false;
-
-//      this options allow android to claim the bitmap memory if it runs low on memory
-        options.inPurgeable = true;
-        options.inInputShareable = true;
-        options.inTempStorage = new byte[16 * 1024];
-
-        try {
-//          load the bitmap from its path
-            bmp = BitmapFactory.decodeFile(filePath, options);
-        } catch (OutOfMemoryError exception) {
-            exception.printStackTrace();
-
-        }
-        try {
-            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888);
-        } catch (OutOfMemoryError exception) {
-            exception.printStackTrace();
-        }
-
-        float ratioX = actualWidth / (float) options.outWidth;
-        float ratioY = actualHeight / (float) options.outHeight;
-        float middleX = actualWidth / 2.0f;
-        float middleY = actualHeight / 2.0f;
-
-        Matrix scaleMatrix = new Matrix();
-        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY);
-
-        Canvas canvas = new Canvas(scaledBitmap);
-        canvas.setMatrix(scaleMatrix);
-        canvas.drawBitmap(bmp, middleX - bmp.getWidth() / 2, middleY - bmp.getHeight() / 2, new Paint(Paint.FILTER_BITMAP_FLAG));
-
-//      check the rotation of the image and display it properly
-        ExifInterface exif;
-        try {
-            exif = new ExifInterface(filePath);
-
-            int orientation = exif.getAttributeInt(
-                    ExifInterface.TAG_ORIENTATION, 0);
-            Log.d("EXIF", "Exif: " + orientation);
-            Matrix matrix = new Matrix();
-            if (orientation == 6) {
-                matrix.postRotate(90);
-                Log.d("EXIF", "Exif: " + orientation);
-            } else if (orientation == 3) {
-                matrix.postRotate(180);
-                Log.d("EXIF", "Exif: " + orientation);
-            } else if (orientation == 8) {
-                matrix.postRotate(270);
-                Log.d("EXIF", "Exif: " + orientation);
-            }
-            scaledBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0,
-                    scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix,
-                    true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        FileOutputStream out = null;
-        String filename = getFilename();
-        try {
-            out = new FileOutputStream(filename);
-
-//          write the compressed bitmap at the destination specified by filename.
-            scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return filename;
-
-    }
-
-    public String getFilename() {
-        File file = new File(Environment.getExternalStorageDirectory().getPath(), "MyFolder/Images");
-        if (!file.exists()) {
-            file.mkdirs();
-        }
-        String uriSting = (file.getAbsolutePath() + "/" + System.currentTimeMillis() + ".jpg");
-        return uriSting;
-
-    }
-
-    private String getRealPathFromURI(String contentURI) {
-        Uri contentUri = Uri.parse(contentURI);
-        Cursor cursor = getActivity().getContentResolver().query(contentUri, null, null, null, null);
-        if (cursor == null) {
-            return contentUri.getPath();
-        } else {
-            cursor.moveToFirst();
-            int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-            return cursor.getString(index);
-        }
-    }
-
-    public int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-            final int heightRatio = Math.round((float) height / (float) reqHeight);
-            final int widthRatio = Math.round((float) width / (float) reqWidth);
-            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
-        }
-        final float totalPixels = width * height;
-        final float totalReqPixelsCap = reqWidth * reqHeight * 2;
-        while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
-            inSampleSize++;
-        }
-
-        return inSampleSize;
     }
 
     public class getAccount extends AsyncTask<Void, Void, ChatListItem> {
@@ -502,7 +300,7 @@ public class Profile extends Fragment {
             if (result != null) {
                 Ipath = result.getContactImg();
                 Glide.with(getActivity())
-                        .load("http://new.opaybot.ir" + Ipath.replace("\"",""))
+                        .load("http://new.opaybot.ir" + Ipath.replace("\"", ""))
                         .asBitmap()
                         .centerCrop()
                         .placeholder(R.drawable.pic_profile)
@@ -540,5 +338,7 @@ public class Profile extends Fragment {
         super.onSaveInstanceState(outState);
     }
 
-
 }
+
+
+
